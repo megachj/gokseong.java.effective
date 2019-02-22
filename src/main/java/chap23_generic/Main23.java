963524bd87f072cee64ef021ab04a3ab4bac9fd5
최해진/게임플랫폼서버팀 <megachj@nhnent.cm>
2019-02-22 18:00:03 +0900

    2019-02-22

--- src/main/java/chap23_generic/Main23.java
+++ src/main/java/chap23_generic/Main23.java
@@ -1,16 +1,30 @@
 package chap23_generic;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main23 {
     public static void main(String[] args) {
         List<String> strings = new ArrayList<>();
 
         unsafeAdd(strings, new Integer(42));
-        // safeAdd(strings, new Integer(45));
+//        safeAdd(strings, new Integer(45));
 
-        String s = strings.get(0);
+//        String s = strings.get(0);
+
+        Set<String> s1 = new HashSet<>(Arrays.asList(new String[]{"aaa", "bbb", "ccc"}));
+        Set<String> s2 = new HashSet<>(Arrays.asList(new String[]{"aaa", "bbb"}));
+
+        int number = numElementsInCommon(s1, s2);
+        int number2 = numElementsInCommonWithUnbounded(s1, s2);
+        System.out.println(number);
+        System.out.println(number2);
+
+        System.out.println(s1.size());
+
+        // 무인자 자료형에서 넣은 숫자때문에 ClassCastException이 발생
+        for(String word: s1) {
+            System.out.println(word);
+        }
     }
 
     // 무인자 자료형 경우는 컴파일 시점에, 타입 체크를 하지 않는다.
@@ -23,4 +37,30 @@ public class Main23 {
         list.add(o);
     }
 
+    // 무인자 자료형, 안전하지 않음
+    private static int numElementsInCommon(Set s1, Set s2) {
+        int result = 0;
+        for(Object o1: s1)
+            if (s2.contains(o1))
+                result++;
+
+        // 무인자 자료형은 아무 자료형이나 넣을 수 있다. 즉 안정성이 없다.
+        s1.add(2);
+
+        return result;
+    }
+
+    // 비한정적 와일드카드 자료형, 형 안정성과 유연성 만족
+    private static int numElementsInCommonWithUnbounded(Set<?> s1, Set<?> s2) {
+        int result = 0;
+        for(Object o1: s1)
+            if (s2.contains(o1))
+                result++;
+
+        // 비한정 와일드카드 자료형은 null 이외의 값은 넣을 수 없다.
+        //s1.add(2); // 컴파일에러
+        s1.add(null);
+
+        return result;
+    }
 }
