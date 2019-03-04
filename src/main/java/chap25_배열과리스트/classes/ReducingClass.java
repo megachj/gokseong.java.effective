7b8d02f28fdf1a8cadf5088c7990dc32ee86d270
최해진/게임플랫폼서버팀 <megachj@nhnent.cm>
2019-03-04 18:11:43 +0900

    2019-03-04

---
+++ src/main/java/chap25_배열과리스트/classes/ReducingClass.java
@@ -0,0 +1,12 @@
+package chap25_배열과리스트.classes;
+
+import chap25_배열과리스트.interfaces.Function;
+
+import java.util.List;
+
+public class ReducingClass {
+
+    static Object reduce(List list, Function f, Object initValue) {
+
+    }
+}
---
+++ src/main/java/chap25_배열과리스트/interfaces/Function.java
@@ -0,0 +1,5 @@
+package chap25_배열과리스트.interfaces;
+
+public interface Function {
+    Object apply(Object arg1, Object arg2);
+}
