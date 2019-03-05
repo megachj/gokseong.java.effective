7951e12c79087f1c01db08da87fe4c60f774b84c
최해진/게임플랫폼서버팀 <megachj@nhnent.cm>
2019-03-05 13:52:40 +0900

    2019-03-05

--- src/main/java/chap25_배열과리스트/Main25.java
+++ src/main/java/chap25_배열과리스트/Main25.java
@@ -1,5 +1,7 @@
 package chap25_배열과리스트;
 
 public class Main25 {
+    public static void main(String[] args) {
 
+    }
 }
--- src/main/java/chap25_배열과리스트/interfaces/GenericFunction.java
+++ src/main/java/chap25_배열과리스트/interfaces/GenericFunction.java
@@ -1,5 +1,5 @@
 package chap25_배열과리스트.interfaces;
 
-interface GenericFunction<T> {
+public interface GenericFunction<T> {
     T apply(T arg1, T arg2);
 }
